529cd32ba94d7a6e62a5f68b34e94232e0648b10
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-02 22:21:38 -0500

    Update code.js

--- app/assets/code.js
+++ app/assets/code.js
@@ -1,1 +1,12 @@
-alert("hola");
+const app= new (function(){
+    this.tbody= document.getElementById('tbody');
+    this.listar_cancha=()=> {
+        fetch("../controller/listar_cancha.php")
+        .then(res=>res.json())
+        .then((data)=>{
+            console.log(data);
+        })
+        .catch((error)=> console.log(error));
+
+    }
+})
